deeafcf6f2ff21176d525824855b7e6508c19b04
Saubhagya Rai <raisaubhagya@gmail.com>
2023-12-10 12:06:52 +0545

    add migration att for company

--- database/migrations/2023_12_10_061704_create_companies_table.php
+++ database/migrations/2023_12_10_061704_create_companies_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('logo')->nullable();
             $table->timestamps();
         });
     }
